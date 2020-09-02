03ce7bec149b4fe3960e8d995efdd8df694cdca5
Eric Easthope <eric.easthope@me.com>
2020-09-02 15:00:42 -0700

    Add resizable SVG component

---
+++ components/SVG.tsx
@@ -0,0 +1,52 @@
+// components/SVG.tsx
+
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  children?: ReactNode;
+  centerOrigin?: boolean;
+}
+
+const SVG = ({ children, centerOrigin }: Props): JSX.Element => {
+  return (
+    <ScalingSVGContainer>
+      <ScalingSVG
+        preserveAspectRatio="xMidYMid meet"
+        viewBox={`-${centerOrigin ? 50 : 0} -${centerOrigin ? 50 : 0} 100 100`}
+      >
+        {children}
+      </ScalingSVG>
+    </ScalingSVGContainer>
+  );
+};
+
+export default SVG;
+
+const ScalingSVGContainer = styled.div`
+  position: relative;
+  padding-bottom: 100vh;
+  max-height: 100vh;
+  display: flex;
+  flex: 1;
+`;
+
+const ScalingSVG = styled.svg`
+  background-color: transparent;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+
+  text {
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  text::selection {
+    background: none;
+  }
+`;
