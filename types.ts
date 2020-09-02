9df3df09203ed0b4c36397b9bd70dc37b27a7644
Eric Easthope <eric.easthope@me.com>
2020-09-02 15:03:28 -0700

    Add graph types

---
+++ types.ts
@@ -0,0 +1,34 @@
+// types.ts
+
+import * as d3 from 'd3';
+
+export interface D3Graph {
+  nodes: D3Node[];
+  links: D3Link[];
+}
+
+export interface D3Node extends d3.SimulationNodeDatum {
+  id: string;
+  group: number;
+}
+
+export interface D3Link extends d3.SimulationLinkDatum<D3Node> {
+  source: string;
+  target: string;
+  value: number;
+}
+
+export interface D3Word extends d3.SimulationNodeDatum {
+  id: string;
+  size: string;
+  align: string;
+  uni: string;
+  width: number;
+}
+
+export interface EdgeNode<Type> {
+  node: {
+    frontmatter: { [Property in keyof Type]: Type[Property] };
+    excerpt?: string;
+  };
+}
